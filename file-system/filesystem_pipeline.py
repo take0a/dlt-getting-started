5f29053e427afe584a828f175b945e290136f4ea
ARAKI Takeo <araki.takeo@robon.co.jp>
2025-06-25 05:53:46 +0000

    PythonとRuff拡張を入れて、不要なコードを整理

--- file-system/filesystem_pipeline.py
+++ file-system/filesystem_pipeline.py
@@ -3,7 +3,11 @@ from dlt.sources.filesystem import filesystem, read_csv
 
 files = filesystem(bucket_url="gs://filesystem-tutorial", file_glob="encounters*.csv")
 reader = (files | read_csv()).with_name("encounters")
-pipeline = dlt.pipeline(pipeline_name="hospital_data_pipeline", dataset_name="hospital_data", destination="duckdb")
+pipeline = dlt.pipeline(
+    pipeline_name="hospital_data_pipeline",
+    dataset_name="hospital_data",
+    destination="duckdb",
+)
 
 info = pipeline.run(reader)
 print(info)
