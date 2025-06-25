f93c8216c00e655058dabc37ecfa3cccb4399394
ARAKI Takeo <araki.takeo@robon.co.jp>
2025-06-25 06:25:40 +0000

    uv init の作った main.py

--- file-system/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from file-system!")
-
-
-if __name__ == "__main__":
-    main()
--- sql-database/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from sql-database!")
-
-
-if __name__ == "__main__":
-    main()
